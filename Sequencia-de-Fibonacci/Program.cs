6aa30c8380adf944a2a023801c5d23022c1551fc
Davy de Lima Carvalho <davy.carvalho@ambevtech.com.br>
2022-01-10 19:28:07 -0300

    Sequencia-de-Fibonacci-em-C-Sharp

---
+++ Sequencia-de-Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Sequencia_de_Fibonacci
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 1;
+            var y = 0; 
+            var z = 0;
+
+            var inputNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputNumber; i++)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                y = x;
+                x = z;
+            }
+            Console.ReadLine();
+        }
+    }
+}
